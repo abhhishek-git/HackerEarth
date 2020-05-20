78b9fd880e01a08c4d309daa13149d0716014b21
Abhishek Singh <abhiwanttochat@gmail.com>
2020-05-20 17:15:05 +0530

    Basic IO using Scanner

--- HackerEarthProblems/src/basicProgramming/BasicInputOutput.java
+++ HackerEarthProblems/src/basicProgramming/BasicInputOutput.java
@@ -3,6 +3,7 @@ package basicProgramming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class BasicInputOutput {
 	public static void main(String[] args) throws IOException {
@@ -11,6 +12,11 @@ public class BasicInputOutput {
 		BufferedReader br = new  BufferedReader(new InputStreamReader(System.in));
 		String name = br.readLine();
 		System.out.println("Hi, "+ name);
+		
+		//Scanner
+		Scanner sc = new Scanner(System.in);
+		String name2 = sc.nextLine();
+		System.out.println("Hi, "+ name2);
 		
 	}
 }
