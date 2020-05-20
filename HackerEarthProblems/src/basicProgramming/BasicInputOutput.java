3e662f62196539f2d72a4b7cdce1d95f5a532a43
Abhishek Singh <abhiwanttochat@gmail.com>
2020-05-20 18:40:19 +0530

    Basic IO

--- HackerEarthProblems/src/basicProgramming/BasicInputOutput.java
+++ HackerEarthProblems/src/basicProgramming/BasicInputOutput.java
@@ -1,22 +1,16 @@
 package basicProgramming;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class BasicInputOutput {
 	public static void main(String[] args) throws IOException {
 		
-		//BufferedReader
-		BufferedReader br = new  BufferedReader(new InputStreamReader(System.in));
-		String name = br.readLine();
-		System.out.println("Hi, "+ name);
-		
-		//Scanner
 		Scanner sc = new Scanner(System.in);
-		String name2 = sc.nextLine();
-		System.out.println("Hi, "+ name2);
+        int i = sc.nextInt();
+        String s = sc.next();
+        System.out.println(i*2);
+        System.out.println(s);
 		
 	}
 }
